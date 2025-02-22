51f157102c32fc4d1af872b14c7237e489cb00bb
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-02-23 03:21:47 +0500

    Create Task2.java

---
+++ OOP_LAB_01/Task2.java
@@ -0,0 +1,14 @@
+// Write a java program to add, sub, multiply and division.
+
+public class Task_02 {
+   public static void main(String[] args){
+       int a = 5;
+       int b = 10;
+       int c = 15;
+       int sum = a + b + c;
+       int multiplication = a * b * c;
+       float division = c/(float)(b/a);
+       System.out.println("The Sum is: " + sum + "\nThe Multiplication is: " + multiplication);
+       System.out.println("The Division is: "+ division);
+   }
+}
