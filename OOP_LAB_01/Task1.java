455e57ffc0b2e09cb4206148c74e2b0c14645e95
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-02-23 03:21:09 +0500

    Create Task1.java

---
+++ OOP_LAB_01/Task1.java
@@ -0,0 +1,9 @@
+
+// Write a java program to display your name, id and email.
+
+public class Lab_01 {
+    public static void main(String[] args) {
+        System.out.println("Name: Shehraz Sarwar\nID: 29261\nEmail: sheeraz23445@gmail.com");
+    }
+   
+}
